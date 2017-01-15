c61314b1b82163aa2164e7d290a67bfe0ffb254f
Joshua Tate <jmtate@icloud.com>
2017-01-15 01:07:22 -0700

    refactored roman_numerals a bit

--- roman_numerals.py
+++ roman_numerals.py
@@ -2,27 +2,23 @@
 
 def checkio(n):
     s = ''
-    special = {
-        4: 'IV', 9: 'IX', 40: 'XL',
-        90: 'XC', 400: 'CD', 900: 'CM'
+    romans = {
+        1: 'I', 4: 'IV', 5: 'V',
+        9: 'IX', 10: 'X', 40: 'XL',
+        50: 'L', 90: 'XC', 100: 'C',
+        400: 'CD', 500: 'D', 900: 'CM', 1000: 'M'
     }
-    regular = {
-        1: 'I', 5: 'V', 10: 'X',
-        50: 'L', 100: 'C',
-        500: 'D', 1000: 'M'
-    }
-    max_tenth = 10 ** (len(str(n)) - 1)
     while n > 0:
+        max_tenth = 10 ** (len(str(n)) - 1)
         digit = n // max_tenth
         if 5 <= digit < 9:
-            rem = (digit * max_tenth) % (5 * max_tenth)
-            s += regular[5 * max_tenth] + checkio(rem)
+            rem = (digit % 5) * max_tenth
+            s += romans[5 * max_tenth] + checkio(rem)
         elif digit == 4 or digit == 9:
-            s += special[digit * max_tenth]
+            s += romans[digit * max_tenth]
         else:
-            s += regular[max_tenth] * digit
+            s += romans[max_tenth] * digit
         n %= max_tenth
-        max_tenth //= 10
     return s
 
 if __name__ == '__main__':
