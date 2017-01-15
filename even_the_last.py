ef51d5314142206b4aab3109175059ec7d4e4e17
Joshua Tate <jmtate@icloud.com>
2017-01-15 16:27:01 -0700

    changed even_to_last to one-liner

--- even_the_last.py
+++ even_the_last.py
@@ -5,10 +5,7 @@ from functools import reduce
 
 
 def checkio(ns):
-    if ns:
-        return reduce((lambda x, y: x + y), ns[::2]) * ns[-1]
-    else:
-        return 0
+    return reduce((lambda x, y: x + y), ns[::2]) * ns[-1] if ns else 0
 
 print(checkio([0, 1, 2, 3, 4, 5]) == 30)
 print(checkio([1, 3, 5]) == 30)
