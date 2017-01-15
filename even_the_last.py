1fc7c707eae41c88b48f0c0fd38922450bfc3d9f
Joshua Tate <jmtate@icloud.com>
2017-01-15 12:36:17 -0700

    solved even_the_last

---
+++ even_the_last.py
@@ -0,0 +1,16 @@
+# You are given an array of integers.
+# You should find the sum of the elements with even indexes
+# then multiply this summed number and the final element
+from functools import reduce
+
+
+def checkio(ns):
+    if ns:
+        return reduce((lambda x, y: x + y), ns[::2]) * ns[-1]
+    else:
+        return 0
+
+print(checkio([0, 1, 2, 3, 4, 5]) == 30)
+print(checkio([1, 3, 5]) == 30)
+print(checkio([6]) == 36)
+print(checkio([]) == 0)
